91eee253038dc4e18be05776af7016d180ae2ccf
raswer4 <55826241+raswer4@users.noreply.github.com>
2021-02-18 14:46:57 +0100

    lol

--- lab09.py
+++ lab09.py
@@ -5,50 +5,52 @@ import zlib
 def readFile():
     file = open('exempeltextMac.txt', 'r')
     txt = file.read()
-    #print("Symbol Len: --> {}".format(len(txt)))
     return bytearray(str(txt),'utf-8')
 
-def makeHisto(array):
+def makeHisto(byteArr):
     hist = {}
     #Get The ASCII Code For Each Element in 'byteArr'
-    for i in array:
+    for i in byteArr:
         hist[i] = hist.get(i , 0) + 1
     #hist: --> Contains value for each characters is repeated.
     return hist
 
-def makeProb(hist):
+def makeProb(histo):
     histSum = 0
     probSum = 0     
     #Get Sum of all the content    
-    for key in hist.keys(): 
-        histSum += hist[key]
+    for key in histo.keys(): 
+        histSum += histo[key]
     #Add the content into a histProb List
-    for key in hist.keys():
-        hist[key] = hist[key] / histSum 
+    for key in histo.keys():
+        histo[key] = histo[key] / histSum 
         # print('ProbKey: --> {:.5f}'.format(hist[key]))
 
-    print("Sum of hist: --> {} ".format(histSum))
-    for key in hist.keys():
-        probSum += hist[key]
-    print("Sum of Prob: --> {} ".format(probSum))
-    return hist
+    for key in histo.keys():    
+        probSum += histo[key]
+
+    print("Sum of hist: {} ".format(histSum))
+    
+    print("Sum of Prob: {} ".format(probSum))
+    return histo
 
 def entropi(prob):
     ent = 0
     for key in prob.keys():
         if prob[key] > 0:
             ent += prob[key] * math.log2(1/prob[key])
-    print("Entropi: --> {}".format(ent))
+    print("Entropi: {}".format(ent))
     return ent
+
 def copyByteArr(byteArr):
     return byteArr.copy()
 
 def shuffledByteArray(byteArr): 
-    copy = copyByteArr(byteArr)
-    decodedCopy = copy.decode('utf-8')
-    copyList = list(decodedCopy)
-    random.shuffle(copyList)
-    return copyList
+    copiedArr = copyByteArr(byteArr)
+    decodedCopiedArr = copiedArr.decode('utf-8')
+    copiedList = list(decodedCopiedArr)
+    random.shuffle(copiedList)
+    return copiedList
 
 def compress(byteArr):
     copy = copyByteArr(byteArr) 
@@ -60,14 +62,14 @@ histogram = makeHisto(fileContent)
 prob = makeProb(histogram)
 ent = entropi(prob)
 Shuffle = shuffledByteArray(fileContent)
-print("===== Shuffled ====== ")
+print("⇓⇓⇓⇓ Shuffled ⇓⇓⇓⇓ ")
 histo = makeHisto(Shuffle)
 prob1 = makeProb(histo)
 entropi = entropi(prob1)
 
 
-# print('Shuffle: --> {}'.format(Shuffle))
-# t1 = """ I hope this lab never ends because it is so incredubly thrilling!""" #66 Bites After It Has Been Compressed
+# print('Shuffle: {}'.format(Shuffle))
+# t1 = #66 Bites After It Has Been Compressed
 # t10 = 10 * t1 #76 Bites After It Has Been Compressed
 # byteArr = bytearray(str(t1), "utf-8")
 
